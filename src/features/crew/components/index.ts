a5045b1742b908de828ac392840e8ec0cb5ec018
OriKatash8 <orikatash8@gmail.com>
2026-06-10 12:03:05 +0300

    chore: export SubCategoryRow and CategoryItem from components barrel

--- src/features/crew/components/index.ts
+++ src/features/crew/components/index.ts
@@ -1,4 +1,6 @@
-export { ProjectRequestCard } from './ProjectRequestCard';
+export { SubCategoryRow } from './SubCategoryRow';
+export { CategoryItem } from './CategoryItem';
 export { CategoryAccordion } from './CategoryAccordion';
 export { CrewBasket } from './CrewBasket';
+export { ProjectRequestCard } from './ProjectRequestCard';
 export { ProjectDetailsForm } from './ProjectDetailsForm';
